4dc2a22932f3d8b3b7963b4c299275156b6bc90b
dj3c1t <dj3c1t@free.fr>
2012-06-24 18:06:03 +0200

    le template dist passe de XHTML 1.0 strict a HTML5

--- out/dist/groupe.php
+++ out/dist/groupe.php
@@ -3,8 +3,8 @@
   $data = $this->data();
   if($this->out_config("groupe_view_albums")) $this->set_config(array("out_albums_menu" => ""));
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+?><!DOCTYPE html>
+<html dir="ltr" lang="fr-FR">
   <head>
     <?php require $this->out_file("views/head.php"); ?>
   </head>
--- out/dist/pages.php
+++ out/dist/pages.php
@@ -2,8 +2,8 @@
 
   $data = $this->data();
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+?><!DOCTYPE html>
+<html dir="ltr" lang="fr-FR">
   <head>
     <?php require $this->out_file("views/head.php"); ?>
   </head>
